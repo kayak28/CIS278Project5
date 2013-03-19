e53f07dde377ab985ce55b19f6eb02cff8ab8026
28kayak <kaya_ota@msn.com>
2013-03-19 15:50:55 -0700

    intial commit

---
+++ HugeInt.h
@@ -0,0 +1,24 @@
+#include <ostream>
+using namespace std;
+class HugeInt 
+{
+	public:
+		HugeInt();
+		HugeInt(string value);
+		
+		int numDigits() const;
+		int operator[] (int n);
+		HugeInt operator ++();
+		HugeInt operator ++(int);
+		bool operator ==(const HugeInt& operand) const;
+		bool operator !=(const HugeInt& operand) const;
+		friend ostream& operator <<(ostream& out, const HugeInt& obj);
+		friend HugeInt operator+(const HugeInt& operend1, const HugeInt& obj);
+		static bool allDigits(string str);
+
+	private:
+		int digits[];
+		int length;
+	
+
+};
