2a2a0b4d06e8ae81e45cbefe12644177cb805cff
28kayak <kaya_ota@msn.com>
2013-03-24 23:13:22 -0700

    add function detail

--- HugeInt.cpp
+++ HugeInt.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 HugeInt::HugeInt()
 {
+	length = 4;
 	for(int i = digit.length(); i >= 0; i--)
 	{
 		if(i < 4)
@@ -21,32 +22,54 @@ HugeInt::HugeInt()
 }
 HugeInt::HugeInt(string value)
 {
+	for(int n = 0; n < SIZE; n++)
+	{//initialize array 
+	 //in order to avoid to enter unknow value automatically 
+		digit[n] = 0;
+	}
 	int scalar; 
 	if(allDigit(value))
 	{
 		scalar = atoi(value);
 		for(int i = value.length()-1; i >= 0; i--)
 		{
-			digit[i] =(int)( scalar/pow(10, i) )% 10;
+			digit[i] =(int)(scalar/pow(10, i) )% 10;
 			//insrat each digit into array with back word..
+			length++; 
 		}
 	}
 
 }
 int HugeInt::numDigits() const
 {//return the num of digits in this HugeInt array 
-
+	
+	return length; 
 
 } 
 int HugeInt::operator[] (int n)
 {
-	return digit[n];
+	if(n >= 0 && n < length)
+	{
+		return digit[n];
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 HugeInt HugeInt::operator++()
 {//operator overloading with pre-increment 
+	HugeInt preIncrement;
+	int index = 0;	
 	
-
+	preIncrement.digit[index] = this.digit[index] + 1;
+	
+	while(preIncrement.digit[index] == 10 && index < length)
+	{
+		preincrement.digit[index + i] = this.digit[i] + 1;
+		index++;  
+	}
 }
 HugeInt HugeInt::operator++(int)
 {//opeartor overloading with post-increment 
@@ -86,8 +109,17 @@ friend ostream& HugeInt::operator <<(ostream& out, const HugeInt& obj)
 	out << "HugeInt: " << hugeInt; 
 }
 friend HugeInt operator+ (const HugeInt& operand1, const HugeInt& operand2)
-{
-	
+{	
+	HugeInt sumInt;
+	for(int i = 0; i < length ; i++)
+	{
+		sumInt.digit[i] = opeeand1.digit[i] + operand2.digit[i];
+		if(sumInt.digit[i] >= 10)
+		{
+			sumInt.digit[i] = (int) (sumInt.digit[i] % 10);
+			sumInt.digit[i+1] = (int)(sumInt.digit[i]/10);
+		}
+	}
 }
 static bool allDigits(string str)
 {
