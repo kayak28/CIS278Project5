b7fa6184ee50a9d09bcfc8a38da8a14eb2cd9a8f
28kayak <kaya_ota@msn.com>
2013-03-24 17:19:00 -0700

    add functions

--- HugeInt.cpp
+++ HugeInt.cpp
@@ -6,14 +6,36 @@ using namespace std;
 
 HugeInt::HugeInt()
 {
+	for(int i = digit.length(); i >= 0; i--)
+	{
+		if(i < 4)
+		{
+			digit[i] = 5;
+		}
+		else
+		{
+			digit[i] = 0;
+		}
 
+	}
 }
 HugeInt::HugeInt(string value)
 {
+	int scalar; 
+	if(allDigit(value))
+	{
+		scalar = atoi(value);
+		for(int i = value.length()-1; i >= 0; i--)
+		{
+			digit[i] =(int)( scalar/pow(10, i) )% 10;
+			//insrat each digit into array with back word..
+		}
+	}
 
 }
 int HugeInt::numDigits() const
-{
+{//return the num of digits in this HugeInt array 
+
 
 } 
 int HugeInt::operator[] (int n)
@@ -22,32 +44,59 @@ int HugeInt::operator[] (int n)
 }
 
 HugeInt HugeInt::operator++()
-{//operator overloadinf with pre-increment 
-
+{//operator overloading with pre-increment 
+	
 
 }
-HugeInt HugeInt::operator(int)
+HugeInt HugeInt::operator++(int)
 {//opeartor overloading with post-increment 
 
 
 }
 bool HugeInt::operator==(const HugeInt& operand)const
-{
+{ 
+	for(int k = 0; k < SIZE; k++)
+	{
+		if(this.digit[k] != operand.digit[k])
+		{
+			return false;
+		}
+	}
+	return true; 
 
 }
 bool HugeInt::operator!= (const HugeInt& operand)const
 {
-
+	for(int index = 0; index < SIZE; index++)
+	{
+		if(this.digit[index] == operand.digit[index])
+		{
+			return false;
+		}
+	}
+	return true;
 }
 friend ostream& HugeInt::operator <<(ostream& out, const HugeInt& obj)
 {
-
+	int hugeInt = 0;//store actual value of integer. 
+	for(int n = digit.length()-1; n >= 0; n--)
+	{
+		hugeInt += digit[n] * pow(10,n); 
+	}
+	out << "HugeInt: " << hugeInt; 
 }
 friend HugeInt operator+ (const HugeInt& operand1, const HugeInt& operand2)
 {
-
+	
 }
 static bool allDigits(string str)
 {
-	if(isDigit())
+	for(int i = 0; i < str.length(); i++)
+	{
+		if(!isDigit(str[i]))
+		{
+			return false;	
+		}
+	}
+	return true; 
 }
