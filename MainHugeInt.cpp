18506ba4ead8b520bae827e2174baa885585d7af
28kayak <kaya_ota@msn.com>
2013-03-26 11:23:22 -0700

    add main file

---
+++ MainHugeInt.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "HugeInt.h"
+using namespace std;
+
+int main()
+{
+	HugeInt bigInt1();
+	HugeInt bigInt2("999");
+
+	cout << bigInt1;
+
+
+
+
+
+
+
+	return 0;
+}//main
